1d69e66b2b7fcc4bde4388949b42cedf70c1b433
Anker579 <angushoward03@gmail.com>
2023-04-18 20:27:31 +0100

    added rpi page with connector

---
+++ db_data_fetcher.py
@@ -0,0 +1,58 @@
+import MySQLdb
+import datetime as dt
+from dotenv import load_dotenv
+import os
+import pandas as pd
+import plotly.express as px
+# from dotenv import dotenv_values
+# config = dotenv_values(".env")
+
+class DBCommunicator():
+
+    def __init__(self) -> None:
+        self.HOST = os.environ.get("HOST")
+        self.DATABASE = os.environ.get("DATABASE")
+        self.USER = os.environ.get("USER")
+        self.PASSWORD = os.environ.get("PASSWORD")
+
+    def db_connector(self):
+        db = MySQLdb.connect(host=self.HOST,
+            database=self.DATABASE,
+            user=self.USER,
+            password=self.PASSWORD)
+        return db
+
+    def create_cursor(self, db):
+        curs = db.cursor()
+        return curs
+    
+    def create_sql_string(self):
+        sql = ("SELECT time, temp_c FROM pitsford_weather_scrape")        
+        return sql
+    
+    def create_df(self, results):
+        df = pd.DataFrame(results, columns=['time', 'temp_c'])
+        return df
+    
+    # def create_data_tuple(self, data_dict):
+    #     '''example data: data = (time, 10, 8, "cold", 51, 8)'''
+    #     time = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    #     temp_c = data_dict["temp_c"]
+    #     feelslike_c = data_dict["feelslike_c"]
+    #     wx_desc = data_dict["wx_desc"]
+    #     lat = data_dict["lat"]
+    #     long = data_dict["long"]
+    #     data = (time, temp_c, feelslike_c, wx_desc, lat, long)
+    #     return data
+
+
+if __name__ == "__main__":
+    my_db_comm = DBCommunicator()
+    db = my_db_comm.db_connector()
+    curs = my_db_comm.create_cursor(db=db)
+    sql = my_db_comm.create_sql_string()
+    curs.execute(sql)
+    result = db.fetchall()
+    df = my_db_comm.create_df(results=result)
+    fig = px.scatter(df, x='time', y='temp_c', title='Temperature vs Time')
+    fig.show()
